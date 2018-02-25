d1cefcd566ae2018f6202bf0e06807a321cebb04
IMAI Hiroei <hre.bz.i@gmail.com>
2018-02-25 17:30:21 +0900

    Fix

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-(cd ../qmk_firmware/keyboards/ergodox_ez/keymaps && ln -s ../../../../ergodox-ez-hre hre)
+#!/bbin/sh
+
+cd ../qmk_firmware/keyboards/ergodox_ez/keymaps && \
+    ( rm -f hre; ln -s ../../../../ergodox-ez-hre/ hre )
+
+sudo apt install -y avr-libc
