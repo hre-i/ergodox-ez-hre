034cf8b3c9112242e3d395887fa319212d4f4692
IMAI Hiroei <hre.bz.i@gmail.com>
2018-03-30 13:54:45 +0900

    Update for GNOME Workspace.

--- keymap.c
+++ keymap.c
@@ -109,20 +109,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.       ,------------------------------------------.
  * |  `    |  1  |  2  |  3  |  4  |  5  | INS |       |  6  |  7  |  8  |  9  |  0  |  -  |  =    |
  * |-------+-----+-----+-----+-----+-----------|       |-----+-----+-----+-----+-----+-----+-------|
- * |Alt/Tab|  Q  |  W  |  E  |  R  |  T  | WS_L|       | WS_R|  Y  |  U  |  I  |  O  |  P  |LAlt/\ |
+ * |Alt/Tab|  Q  |  W  |  E  |  R  |  T  |WIN_L|       |WIN_R|  Y  |  U  |  I  |  O  |  P  |LAlt/\ |
  * |-------+-----+-----+-----+-----+-----|     |       |     |-----+-----+-----+-----+-----+-------|
  * | LCtl  |  A  |  S  |  D  |  F  |  G  |-----|       |-----|  H  |  J  |  K  |  L  | ;:  |LCtl/' |
  * |-------+-----+-----+-----+-----+-----| DEL |       | BS  |-----+-----+-----+-----+-----+-------|
  * | LSft  |  Z  |  X  |  C  |  V  |  B  |     |       |     |  N  |  M  |  ,  |  .  | /?  | RSft  |
  * `-------+-----+-----+-----+-----+-----------'       `-----------+-----+-----+-----+-----+-------'
- *  | WS_L | WS_R| INS | L2  | LALT|                               |LALT | L2  |[   |  ]   | L3   |
+ *  | WS_1 | WS_2| INS | L2  | LALT|                               |LALT | L2  |[   |  ]   | L3   |
  *  `------------------------------'                               `------------------------------'
  *                                 ,-------------.   ,-------------.
- *                                 | HELP | PgUp |   | Home | RALT |
+ *                                 | LWIN | PgUp |   | Home | RALT |
  *                          ,------|------|------|   |------+------+------.
  *                          |      |      | PgDn |   | End  |      |      |
  *                          | Ent  | ESC  |------|   |------| L1   | SPC  |
- *                          |      |      | LWIN |   | RWIN |      |      |
+ *                          |      |      | RWIN |   | RWIN |      |      |
  *                          `--------------------'   `--------------------'
  *
  */
@@ -130,17 +130,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             // left hand
             //--+----* ----+----* ----+----* ----+----* ----+----* ----+----* ----+----*
             KC_GRAVE,  KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_INS,
-            KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      WS_L,
+            KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      WIN_L,
             KC_LCTRL,  KC_A,      KC_S,      KC_D,      KC_F,      KC_G,
             KC_LSFT,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_DELETE,
-            WS_L,      WS_R,      KC_INS,    MO(WCTL),  KC_LALT,
-                       KC_HELP,   KC_PGUP,
+            WS_1,      WS_2,      KC_INS,    MO(WCTL),  KC_LALT,
+                       KC_LWIN,   KC_PGUP,
                                   KC_PGDN,
-            KC_ENT,   KC_ESC,     KC_LWIN,
+            KC_ENT,   KC_ESC,     KC_RWIN,
             // right hand
             //--+----* ----+----* ----+----* ----+----* ----+----* ----+----* ----+----*
             KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_MINS,   KC_EQL,
-            WS_R,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      LALT_T(KC_BSLS),
+            WIN_R,     KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      LALT_T(KC_BSLS),
                        KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   LCTL_T(KC_QUOT),
             KC_BS,     KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_RSFT,
                                   KC_LALT,   MO(WCTL),  KC_LBRC,   KC_RBRC,   MO(MDIA),
