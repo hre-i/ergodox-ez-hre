25f9129b011679bb4b0282f6496ca74f23d197ac
IMAI Hiroei <hre.bz.i@gmail.com>
2018-03-31 06:44:16 +0900

    Support arch linux.

--- setup.sh
+++ setup.sh
@@ -3,4 +3,8 @@
 cd ../qmk_firmware/keyboards/ergodox_ez/keymaps && \
     ( rm -f hre; ln -s ../../../../ergodox-ez-hre/ hre )
 
-sudo apt install -y avr-libc
+if   [ -x /usr/bin/apt ]; then
+    sudo apt install -y avr-libc
+elif [ -x /usr/bin/pacman ]; then
+    sudo pacman -S avr-libc
+fi
