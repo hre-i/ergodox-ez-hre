0874739e13c50fc4360053644dbd7b65a77a894a
IMAI Hiroei <hre.bz.i@gmail.com>
2018-06-19 11:58:28 +0900

    Add git cloen qmk_firmware.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bbin/sh
 
-cd ../qmk_firmware/keyboards/ergodox_ez/keymaps && \
-    ( rm -f hre; ln -s ../../../../ergodox-ez-hre/ hre )
-
 if   [ -x /usr/bin/apt ]; then
     sudo apt install -y avr-libc
 elif [ -x /usr/bin/pacman ]; then
     sudo pacman -S avr-libc
 fi
+
+if [ ! -d ../qmk_firmware ]; then
+    (cd ../; git clone --recursive https://github.com/qmk/qmk_firmware.git)
+fi
+
+cd ../qmk_firmware/keyboards/ergodox_ez/keymaps && \
+    ( rm -f hre; ln -s ../../../../ergodox-ez-hre/ hre )
